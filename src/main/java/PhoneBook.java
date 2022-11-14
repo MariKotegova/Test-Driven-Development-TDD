4f79cea79cb476b7a0306844e2f981d31c0a1401
Котегова Мария <m.kotegowa@yandex.ru>
2022-11-14 13:42:09 +0300

    Stub and tests for add

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -1,2 +1,12 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class PhoneBook {
+    private static Map<String, String> myPhoneBook = new HashMap<>();
+
+    public boolean add (String name, String number){
+        //add - добавить имя с номером. Метод должен возвращать количество
+        // контактов после добавления, при этом гарантируется, что не будут добавляться повторяющиеся имена;
+        return false;
+    }
 }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,2 +1,41 @@
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class PhoneBookTest {
+
+    PhoneBook sut;
+
+    @BeforeAll
+    public static void started(){
+        System.out.println("tests started");
+    }
+
+    @BeforeEach
+    public void init(){
+        System.out.println("test started");
+        sut = new PhoneBook();
+    }
+
+    @AfterEach
+    public void finished(){
+        System.out.println("test compled");
+        sut = null;
+    }
+
+    @AfterAll
+    public static void finishedTest(){
+        System.out.println("tests compled");
+    }
+
+    @Test
+    public void testAdd (){
+        String name = "Petya";
+        String number = "+7 777 777 7777";
+        boolean expected = true;
+        boolean resylt = sut.add(name, number);
+
+        assertEquals(expected, resylt);
+    }
+
 }
