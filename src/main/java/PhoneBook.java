2310834c8ee9f29c631c0185228d306c29384b65
Котегова Мария <m.kotegowa@yandex.ru>
2022-11-14 15:00:25 +0300

    Stub and tests for findByNumber

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -4,13 +4,13 @@ import java.util.Map;
 public class PhoneBook {
     private Map<String, String> myPhoneBook = new HashMap<>();
 
-    public boolean add (String name, String number){
-        if(myPhoneBook.isEmpty()){
+    public boolean add(String name, String number) {
+        if (myPhoneBook.isEmpty()) {
             myPhoneBook.put(name, number);
-            System.out.println("В телефонную книгу добавлен 1 контакт");
+            System.out.println("Добавлен первый контакт");
             return true;
         }
-        if(!myPhoneBook.containsKey(name)){
+        if (!myPhoneBook.containsKey(name)) {
             myPhoneBook.put(name, number);
             System.out.println("Контакт добавлен" + "\nОбщее колличествоконтактов " +
                     myPhoneBook.size());
@@ -20,8 +20,9 @@ public class PhoneBook {
                     myPhoneBook.size());
             return false;
         }
-        //add - добавить имя с номером. Метод должен возвращать количество
-        // контактов после добавления, при этом гарантируется, что не будут добавляться повторяющиеся имена;
+    }
 
+    public String findByNumber(String number) {
+        return null;
     }
 }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,64 +1,88 @@
 import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 public class PhoneBookTest {
 
     PhoneBook sut;
 
     @BeforeAll
-    public static void started(){
+    public static void started() {
         System.out.println("tests started");
     }
 
     @BeforeEach
-    public void init(){
+    public void init() {
         System.out.println("test started");
         sut = new PhoneBook();
     }
 
     @AfterEach
-    public void finished(){
+    public void finished() {
         System.out.println("test compled");
         sut = null;
     }
 
     @AfterAll
-    public static void finishedTest(){
+    public static void finishedTest() {
         System.out.println("tests compled");
     }
 
     @Test
-    public void testAdd (){
+    public void testAdd() {
         String name = "Petya";
         String number = "+7 777 777 7777";
         boolean expected = true;
-        boolean resylt = sut.add(name, number);
+        boolean result = sut.add(name, number);
 
-        assertEquals(expected, resylt);
+        assertEquals(expected, result);
     }
 
     @Test
-    public void testAddFalse (){
+    public void testAddFalse() {
         String name = "Petya";
         String number = "+7 777 777 7777";
         boolean expected = false;
         sut.add(name, number);
-        boolean resylt = sut.add(name, number);
+        boolean result = sut.add(name, number);
 
-        assertEquals(expected, resylt);
+        assertEquals(expected, result);
     }
 
     @Test
-    public void testAddNewContect (){
+    public void testAddNewContect() {
         String name = "Petya";
         String number = "+7 777 777 7777";
         String name2 = "Tanya";
         String number2 = "+7 888 888 8888";
         boolean expected = true;
         sut.add(name, number);
-        boolean resylt = sut.add(name2, number2);
+        boolean result = sut.add(name2, number2);
 
-        assertEquals(expected, resylt);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void testFindByNumber() {
+        String name = "Petya";
+        String number = "+7 777 777 7777";
+        String name2 = "Tanya";
+        String number2 = "+7 888 888 8888";
+        sut.add(name, number);
+        sut.add(name2, number2);
+        String expected = "Petya";
+
+        String result = sut.findByNumber(number);
+
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void testFindByNumberNull() {
+
+        String result = sut.findByNumber("+7 666 666 6666");
+
+        assertNull(result);
     }
 }
